36f9aab800acbb7a23624e1c2dfa2c9ef3dd0ed3
3n3a <info@3n3a.ch>
2022-08-13 22:13:03 +0200

    add sitemap library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import prefetch from "@astrojs/prefetch";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch()]
+  site: "https://3n3a.ch",
+  integrations: [prefetch(), sitemap()]
 });
