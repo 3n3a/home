61b39f4886ddf349460a45d072d024ff54423484
3n3a <info@3n3a.ch>
2022-08-13 20:32:36 +0200

    add homepage

---
+++ src/enum/Color.ts
@@ -0,0 +1,5 @@
+export enum Color {
+    "purpleBlue" = "linear-gradient(45deg, #4f39fa, #da62c4 30%, var(--card-border) 60%)",
+    "blueYellow" = "linear-gradient(45deg, #FDBB2D, #22C1C3 30%, var(--card-border) 60%)",
+    "lightBluePurple" = "linear-gradient(45deg, #8EC5FC, #E0C3FC 30%, var(--card-border) 60%)"
+}
