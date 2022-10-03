dcc88ad1faa42444713e03e77a60549ebc5ef6df
3n3a <46775561+3n3a@users.noreply.github.com>
2022-10-03 07:42:43 +0200

    add correct api path

--- functions/api/weather.js
+++ functions/api/weather.js
@@ -3,7 +3,7 @@
 export async function onRequestGet({ request, env }) {
   const latitude = request.cf.latitude;
   const longitude = request.cf.longitude;
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${env['API_KEY']}`);
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${env['WEATHER_API_KEY']}&units=metric`);
   const data = await res.json();
   const info = JSON.stringify(data, null, 2);
   return new Response(info);
