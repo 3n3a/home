b22d64a67199043a60d778cad25191b653b77840
3n3a <info@3n3a.ch>
2023-09-17 23:00:31 +0200

    add links list on homepage

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
 # TODO
 
-- [x] Add Backend with secret API, to fetch Projects from at build
-- [x] Write the About page
 - [ ] Separate "Projects" site, linked to from "More Projects" on Home
---
+++ src/model/Links.ts
@@ -0,0 +1,16 @@
+export interface LinkTag {
+    name: string
+}
+
+export interface LinkTagExpand {
+    tags: LinkTag[]
+}
+
+export interface Link {
+    id: string
+    title: string
+    notes: string
+    status: string
+    url: string
+    expand: LinkTagExpand[]
+}
