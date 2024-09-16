cacfce6d607869a31ca1d855036c29aee82c60a6
3n3a <46775561+3n3a@users.noreply.github.com>
2024-09-16 20:43:30 +0200

    fix to make it work

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,6 @@
-export async function getToken(env=import.meta.env) {
-    const { API_URL, API_USERNAME, API_PASSWORD } = env;
+const { API_URL, API_USERNAME, API_PASSWORD } = import.meta.env;
 
+export async function getToken() {
     const response = await fetch(`${API_URL}/api/collections/users/auth-with-password`, {
         method: "POST",
         redirect: "follow",
