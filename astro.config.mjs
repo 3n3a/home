0444462a32024cd4385610c1278ef80809b53d91
3n3a <46775561+3n3a@users.noreply.github.com>
2024-09-16 20:51:08 +0200

    remove astro prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import prefetch from "@astrojs/prefetch";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://3n3a.ch",
-  integrations: [prefetch(), sitemap()]
+  integrations: [sitemap()]
 });
