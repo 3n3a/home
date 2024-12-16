51dfa8ead602b72f31fb006209b1463843c40de3
3n3a <46775561+3n3a@users.noreply.github.com>
2024-12-16 22:06:02 +0100

    add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://3n3a.ch",
-  integrations: [sitemap()]
+  integrations: [sitemap(), mdx()]
 });
