888cc1220efb97635fef39307f4b0a76c47bcc01
3n3a <46775561+3n3a@users.noreply.github.com>
2025-01-30 12:44:32 +0100

    split heart emojis by each page

--- functions/api/heart.js
+++ functions/api/heart.js
@@ -4,21 +4,37 @@
  * Source: openheart.fyi
  */
 
-export async function onRequestGet({ env }) {
+function getHeartId(request) {
+    const referer = request.headers.get('referer');
+    const id = btoa(referer);
+    return id;
+}
+
+export async function onRequestGet({ request, env }) {
+    const id = getHeartId(request);
+
     let hearts = {};
-    const heartsList = await env.HEARTS.list();
+    const heartsList = await env.HEARTS.list({prefix: `${id}-`});
+
+    if (heartsList.keys.length === 0) {
+        hearts['❤️'] = 0;
+    }
+
     for (const heartElement of heartsList.keys) {
         const count = Number(await (env.HEARTS.get(heartElement.name)) || 0);
-        hearts[`${heartElement.name}`] = count;
+        const [id, emoji] = heartElement.name.split("-");
+        hearts[emoji] = count;
     }
     return Response.json(hearts);
 }
 
 export async function onRequestPost({ request, env }) {
+    const id = getHeartId(request);
+
     const emoji = ensureEmoji(await request.text());
     if (!emoji) return Response.json({status: 'not ok'}, { status: 500 });
 
-    const key = `${emoji}`;
+    const key = `${id}-${emoji}`;
     // https://developers.cloudflare.com/workers/runtime-apis/kv/
     const currentCount = Number(await (env.HEARTS.get(key)) || 0);
     const newCount = currentCount + 1;
